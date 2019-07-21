82700ed2578c180cc0a67b006220b176707542dd
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-07-21 14:28:57 -0500

    loader componente created to show when user make request to api

---
+++ src/components/loaders/LoaderComponent.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="vld-parent">
+        <loading 
+            :active.sync="isLoading" 
+            :is-full-page="fullPage"
+            :color='color'
+            :loader='loader'
+            >
+        </loading>
+    </div>
+</template>
+
+<script>
+    // Import component
+    import Loading from 'vue-loading-overlay';
+    // Import stylesheet
+    import 'vue-loading-overlay/dist/vue-loading.css';
+    
+    export default {
+        data() {
+            return {
+                fullPage: true,
+                loader:'spinner',
+                color: '#2196f3'
+            }
+        },
+        components: {
+            Loading
+        },
+        computed: {
+            isLoading(){
+                return this.$store.getters['movies/isLoading'];
+            }
+        }
+    }
+</script>
